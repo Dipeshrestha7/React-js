71314559ad24c53df0fe64f37dc6a4cd32a3ecdf
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2024-08-11 14:03:54 +0000

    config

--- 03_tailwind_props/src/App.jsx
+++ 03_tailwind_props/src/App.jsx
@@ -2,32 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj ={
+    username: "Dipesh",
+    age: 21
+  }
 
+  let newArr =[1,2,3,4]
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind Test</h1>
+      {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/27586893/pexels-photo-27586893/free-photo-of-station.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width="384" height="512" />
+      <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p class="text-lg font-medium">
+            “Tailwind CSS is the only framework that I've seen scale
+            on large teams. It’s easy to customize, adapts to any design,
+            and the build size is tiny.”
+          </p>
+        </blockquote>
+        <figcaption class="font-medium">
+          <div class="text-sky-500 dark:text-sky-400">
+            Sarah Dayan
+          </div>
+          <div class="text-slate-700 dark:text-slate-500">
+            Staff Engineer, Algolia
+          </div>
+        </figcaption>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </figure> */}
+      <Card username="Dipesh Shrestha" btnText="click me"  />
+      <Card username="Axel Blaze" btnText="visit me"/>
+      
     </>
   )
 }
