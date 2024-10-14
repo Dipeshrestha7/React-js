d136d7300274ee8b0ac5672897b11b389e266eb9
Dipesh Shrestha <139684493+Dipeshrestha7@users.noreply.github.com>
2024-10-14 03:38:20 +0000

    config

--- Website/src/Page.jsx
+++ Website/src/Page.jsx
@@ -4,7 +4,7 @@ function Page() {
     return(
         <div className="bg-green-500 flex items-center justify-center ">
         <a href="/About">About</a>
-        <a href="/Cources">Cources</a>
+        <a href="/Cources">Courses</a>
         <a href="/Login">Login</a>
         </div>
     )
